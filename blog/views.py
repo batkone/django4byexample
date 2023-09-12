2ffb9ec0481e320ffc229bd7a3ecf537003fe2c9
Yavor <yavor@yavor.net>
2023-09-12 21:50:05 +0200

    adding the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
 # Create your views here.
 
+def post_share(request, post_id):
+	# Retrieve post by id
+	#post = get_object_or_404(Post, id=post_id, status=Post.status.published)
+	post = get_object_or_404(Post, id=post_id)
+	sent = False
+
+	if request.method == 'POST':
+	   # Form was submitted
+	   form = EmailPostForm(request.POST)
+	   if form.is_valid():
+	   	   # Form field passed validation
+	   	   cd = form.cleaned_data
+	   	   post_url = request.build_absolute_uri(post.get_absolute_url())
+	   	   subject = f"{cd['name']} recommends you read " f"{ post.title }"
+	   	   message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+	   	   send_mail(subject, message, 'batkone@gmail.com', [cd['to']])
+	   	   sent = True
+	else:
+		form = EmailPostForm()
+	return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent })
 class PostListView(ListView):
 	"""
 	Alternative post list view
@@ -29,4 +53,23 @@ def post_list(request):
 
 def post_detail(request,year, month, day, post):
 	post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-	return render(request, 'blog/post/detail.html', {'post': post})
+	# List of active comments for this post
+	comments = post.comments.filter(active=True)
+	# Form for user to comment
+	form = CommentForm()
+	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form })
+
+@require_POST
+def post_comment(request, post_id):
+	post = get_object_or_404(Post, id=post_id, status='published')
+	comment = None
+	# A comment was posted
+	form = CommentForm(data=request.POST)
+	if form.is_valid():
+		# Create a Comment object without saving it to the database
+		comment = form.save(commit=False)
+		# Assign the post to the comment
+		comment.post = post
+		# Save the comment to the database
+		comment.save()
+	return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
